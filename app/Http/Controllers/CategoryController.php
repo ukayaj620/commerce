313f062c74fbeebc138a1fe47f697b1591597bd3
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-04-13 04:29:02 +0700

    feat: create category and product controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Category;
+
+
+class CategoryController extends Controller
+{
+
+    public function fetchAll() {
+        $categories = Category::all();
+        return view('admin.category.index', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function create(Request $request) {
+        $request->validate([
+            'category_name' => 'required',
+            'description' => 'required',
+        ]);
+        Category::create($request->all());
+        $categories = Category::all();
+        return redirect('/category');
+    }
+
+    public function update(Request $request) {
+        $request->validate([
+            'id' => 'required',
+            'category_name' => 'required',
+            'description' => 'required',
+        ]);
+        // TODO. Find out why $request->all() can't return a true output
+        Category::whereId($request->input('id'))->update([
+            'category_name' => $request->input('category_name'),
+            'description' => $request->input('description'),
+        ]);
+        return redirect('/category');
+    }
+
+    public function delete($id) {
+        Category::find($id)->delete();
+        return redirect('/category');
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\Categories;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    public function add()
+    {
+        $categories = Categories::all();
+        return view('pages.product.add', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $categories = Categories::all();
+        $product = Product::find($id);
+        return view('pages.product.edit', [
+            'categories' => $categories,
+            'product' => $product,
+        ]);
+    }
+
+    public function fetchAll()
+    {
+        $products = Product::all();
+        return view('pages.product.product', [
+            'products' => $products
+        ]);
+    }
+
+    public function fetchById($id)
+    {
+        $product = Product::find($id);
+        return view('pages.product.view', [
+            'product' => $product
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        // must in the same order with form
+        $request->validate([
+            'product_name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'weight' => 'required',
+            'category' => 'required',
+            'product_rate' => 'required',
+            'description' => 'required',
+            'image' => 'required|file'
+        ]);
+
+        $photo = $request->file('image');
+        $image_path = uniqid() . '.' .$photo->extension();
+        $path = $photo->storeAs('public/', $image_path);
+
+        $product = Product::create([
+            'product_name' => $request->input('product_name'),
+            'price' => $request->input('price'),
+            'stock' => $request->input('stock'),
+            'weight' => $request->input('weight'),
+            'product_rate' => $request->input('product_rate'),
+            'description' => $request->input('description'),
+        ]);
+
+        $product->categories()->attach($request->input('category'));
+
+        ProductImage::create([
+            'product_id' => $product->id,
+            'image_path' => $image_path
+        ]);
+
+        return redirect('/product');
+    }
+
+    public function update(Request $request)
+    {
+        // must in the same order with form
+        $request->validate([
+            'id' => 'required',
+            'product_name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'weight' => 'required',
+            'category' => 'required',
+            'description' => 'required',
+        ]);
+
+        $product = Product::find($request->input('id'));
+
+        Product::whereId($request->input('id'))->update([
+            'product_name' => $request->input('product_name'),
+            'price' => $request->input('price'),
+            'stock' => $request->input('stock'),
+            'weight' => $request->input('weight'),
+            'description' => $request->input('description'),
+        ]);
+
+        $product->categories()->detach($product->categories[0]->id);
+
+        $product->categories()->attach($request->input('category'));
+
+        return redirect('/product');
+    }
+
+    public function delete($id)
+    {
+        Product::find($id)->delete();
+        return redirect('/product');
+    }
+}
