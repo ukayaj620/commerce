122d4696e2770133f7165f7c6a09703c3abd6a78
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-04-13 04:31:38 +0700

    feat: bind controller to route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,4 +37,21 @@ Route::prefix('admin')->group(function (){
 
     Route::get('/register', [RegisterController::class, 'showAdminRegister'])->name('admin.register.form');
     Route::post('/register', [RegisterController::class, 'adminRegister'])->name('admin.register');
+
+    Route::prefix('category')->group(function (){
+        Route::get('/', [CategoryController::class, 'fetchAll'])->name('category.fetchAll');
+        Route::post('/create', [CategoryController::class, 'create'])->name('category.create');
+        Route::put('/update', [CategoryController::class, 'update'])->name('category.update');
+        Route::delete('/delete/{id}', [CategoryController::class, 'delete'])->name('category.delete');
+    });
+
+    Route::prefix('product')->group(function (){
+        Route::get('/', [ProductController::class, 'fetchAll'])->name('product.fetchAll');
+        Route::get('/add', [ProductController::class, 'add'])->name('product.add');
+        Route::post('/create', [ProductController::class, 'create'])->name('product.create');
+        Route::put('/update', [ProductController::class, 'update'])->name('product.update');
+        Route::delete('/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+        Route::get('/fetch/{id}', [ProductController::class, 'fetchById'])->name('product.byId');
+        Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+    })
 });
